47d198a2b08e19adbf9a9a9f29daeb38696af302
alex <cyy0523xc@gmail.com>
2019-03-26 15:51:41 +0800

    face py3

--- face/start.sh
+++ face/start.sh
@@ -4,8 +4,7 @@
 # Author: alex
 # Created Time: 2019年03月13日 星期三 10时49分08秒
 
-sudo docker run --rm --runtime=nvidia -ti \
+sudo docker run --rm --runtime=nvidia -ti --name=ibbd-face \
     -w /face/insightface \
     -v "$PWD"/models:/face/insightface/models \
-    registry.cn-hangzhou.aliyuncs.com/ibbd/face:insightface \
-    /bin/bash
+    "$1" /bin/bash

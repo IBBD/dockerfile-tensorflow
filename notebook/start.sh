2b5505d8a34f41a4dc2e19a910394ea2520a0510
alex <cyy0523xc@gmail.com>
2019-04-26 11:19:19 +0800

    start

--- notebook/start.sh
+++ notebook/start.sh
@@ -4,7 +4,10 @@
 # Author: alex
 # Created Time: 2019年04月26日 星期五 10时50分41秒
 
-    #-v $HOME/notebook/work:/home/jovyan/work \
+#-v $HOME/notebook/work:/home/jovyan/work \
+# TODO 本应该挂载一个数据目录到work的，但是挂载这个目录时候，在容器里的权限就出现读写问题
+# 容器里面的用户是jovyan
+# 没有外部目录的话，容器删除就会丢数据
 docker run -d --restart always --name ibbd-notebook \
     -p 8888:8888 \
     -e JUPYTER_ENABLE_LAB=yes \
